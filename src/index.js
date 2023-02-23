26b573a2cf9000435f919eb1e201b809f698f4c1
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-02-23 11:15:37 -0800

    fix: Use updated render()

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App';
 
 function ColorfulBorder() {
   return (
     <React.Fragment>
-      <ul className='border-container'>
-        <li className='border-item' style={{ background: 'var(--red)' }} />
-        <li className='border-item' style={{ background: 'var(--blue)' }} />
-        <li className='border-item' style={{ background: 'var(--pink)' }} />
-        <li className='border-item' style={{ background: 'var(--yellow)' }} />
-        <li className='border-item' style={{ background: 'var(--aqua)' }} />
+      <ul className="border-container">
+        <li className="border-item" style={{ background: 'var(--red)' }} />
+        <li className="border-item" style={{ background: 'var(--blue)' }} />
+        <li className="border-item" style={{ background: 'var(--pink)' }} />
+        <li className="border-item" style={{ background: 'var(--yellow)' }} />
+        <li className="border-item" style={{ background: 'var(--aqua)' }} />
       </ul>
     </React.Fragment>
-  )
+  );
 }
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <ColorfulBorder />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
