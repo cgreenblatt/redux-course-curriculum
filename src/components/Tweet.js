8818d50424d5314900efb1930e41308efeccf075
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-02-28 10:19:58 -0800

    feat: Add Tweet component

---
+++ src/components/Tweet.js
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { heartIcon, heartIconFilled, replyIcon } from './icons.js';
+import { toggleLikeTweet } from '../actions/tweets';
+
+export default function Tweet({ tweet }) {
+  const author = useSelector((state) => state.users[tweet.author]);
+  const dispatch = useDispatch();
+  const tempDate = new Date(tweet.timestamp);
+  const authedUser = useSelector((state) => state.authedUser);
+  const handleLikeTweet = () => dispatch(toggleLikeTweet(tweet.id, authedUser));
+
+  const initialTweetAuthor = useSelector(({ tweets }) => {
+    return tweet.replyingTo ? tweets[tweet.replyingTo].author : null;
+  });
+  return (
+    <div className="tweet">
+      <img
+        className="avatar"
+        src={author.avatarURL}
+        alt={`${author.name} avatar`}
+      />
+      <div className="tweet-info">
+        <h3>{author.name}</h3>
+        <div className="tweet-info__ts">
+          {`${tempDate.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+          })} | ${tempDate.toLocaleDateString()}`}
+        </div>
+        {initialTweetAuthor && (
+          <div className="tweet-info__reply">
+            Replying to @{initialTweetAuthor}
+          </div>
+        )}
+        <p>{tweet.text}</p>
+        <div className="tweet-icons">
+          {replyIcon}
+          <span>{tweet.replies.length}</span>
+          <button className="heart-button" onClick={handleLikeTweet}>
+            {tweet.likes.includes(authedUser) ? heartIconFilled : heartIcon}
+          </button>
+          <span>{tweet.likes.length}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,12 +1,17 @@
 import { useSelector } from 'react-redux';
+import Tweet from './Tweet';
 
 export default function Tweets() {
   const tweets = useSelector((state) => state.tweets);
+  const sortedTweets = Object.values(tweets).sort(
+    (a, b) => b.timestamp - a.timestamp
+  );
+
   return (
     <ul>
-      {Object.keys(tweets).map((key) => (
-        <li key={key}>
-          <p>{JSON.stringify(tweets[key])}</p>
+      {sortedTweets.map((tweet) => (
+        <li key={tweet.id}>
+          <Tweet tweet={tweet} />
         </li>
       ))}
     </ul>
