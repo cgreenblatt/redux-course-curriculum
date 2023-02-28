38392f8036f0d2963208ec2b5bec3c754f6d4159
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-02-28 10:19:13 -0800

    feat: Add TOGGLE_LIKE_TWEET action

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,17 @@
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const TOGGLE_LIKE_TWEET = 'TOGGLE_LIKE_TWEET';
 
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets,
+  };
+}
+
+export function toggleLikeTweet(tweetId, authedUser) {
+  return {
+    type: TOGGLE_LIKE_TWEET,
+    tweetId,
+    authedUser,
   };
 }
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,9 +1,20 @@
-import { RECEIVE_TWEETS } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_LIKE_TWEET } from '../actions/tweets';
 
 export default function tweetsReducer(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
       return action.tweets;
+    case TOGGLE_LIKE_TWEET:
+      const { tweetId, authedUser } = action;
+      return {
+        ...state,
+        [tweetId]: {
+          ...state[tweetId],
+          likes: state[tweetId].likes.includes(authedUser)
+            ? state[tweetId].likes.filter((userId) => userId !== authedUser)
+            : state[tweetId].likes.concat(authedUser),
+        },
+      };
     default:
       return state;
   }
