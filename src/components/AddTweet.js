08624e6051cfbaf02fd6d7c4bf78c871c514dde3
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-03-01 10:55:27 -0800

    feat: Add functionaltiy for responding to a tweet

--- src/components/AddTweet.js
+++ src/components/AddTweet.js
@@ -1,23 +1,41 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { handleSaveTweet } from '../actions/tweets';
+import Tweet from './Tweet';
+import Tweets from './Tweets';
 
 export default function AddTweet() {
   const [text, setText] = useState('');
+  const { id } = useParams();
+  const tweets = useSelector((state) => state.tweets);
+  const tweet = tweets[id];
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleSetText = (e) => setText(e.target.value);
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      handleSaveTweet({ text, replyingTo: id || null }, () => navigate('/'))
+    );
+  };
 
   return (
-    <form>
-      <textarea
-        className="textarea"
-        placeholder="What's happening?"
-        value={text}
-        onChange={handleSetText}
-      ></textarea>
-      <button className="btn" type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
-    </form>
+    <>
+      {id && <Tweet tweet={tweet} />}
+      <form className="new-tweet">
+        <textarea
+          className="textarea"
+          placeholder="What's happening?"
+          value={text}
+          onChange={handleSetText}
+          maxLength={280}
+        ></textarea>
+        <button className="btn" type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+      {id && tweet.replies.length > 0 && <Tweets selected={tweet.replies} />}
+    </>
   );
 }
