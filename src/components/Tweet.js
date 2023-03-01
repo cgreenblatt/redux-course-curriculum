d58bb6c4fbe2e7e5c5edbaf7fc3afe9d0735f1dd
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-03-01 10:51:47 -0800

    feat: Add Links

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { heartIcon, heartIconFilled, replyIcon } from './icons.js';
 import { handleToggleLike } from '../actions/tweets';
 
@@ -8,42 +9,48 @@ export default function Tweet({ tweet }) {
   const tempDate = new Date(tweet.timestamp);
   const authedUser = useSelector((state) => state.authedUser);
   const liked = tweet.likes.includes(authedUser);
-  const toggleLike = () =>
+  const toggleLike = (e) => {
+    e.preventDefault();
     dispatch(handleToggleLike({ id: tweet.id, hasLiked: !liked, authedUser }));
+  };
 
   const initialTweetAuthor = useSelector(({ tweets }) => {
     return tweet.replyingTo ? tweets[tweet.replyingTo].author : null;
   });
   return (
-    <div className="tweet">
-      <img
-        className="avatar"
-        src={author.avatarURL}
-        alt={`${author.name} avatar`}
-      />
-      <div className="tweet-info">
-        <h3>{author.name}</h3>
-        <div className="tweet-info__ts">
-          {`${tempDate.toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
-          })} | ${tempDate.toLocaleDateString()}`}
-        </div>
-        {initialTweetAuthor && (
-          <div className="tweet-info__reply">
-            Replying to @{initialTweetAuthor}
+    <Link to={`/tweet/${tweet.id}`}>
+      <div className="tweet">
+        <img
+          className="avatar"
+          src={author.avatarURL}
+          alt={`${author.name} avatar`}
+        />
+        <div className="tweet-info">
+          <h3>{author.name}</h3>
+          <div className="tweet-info__ts">
+            {`${tempDate.toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            })} | ${tempDate.toLocaleDateString()}`}
           </div>
-        )}
-        <p>{tweet.text}</p>
-        <div className="tweet-icons">
-          {replyIcon}
-          <span>{tweet.replies.length}</span>
-          <button className="heart-button" onClick={toggleLike}>
-            {liked ? heartIconFilled : heartIcon}
-          </button>
-          <span>{tweet.likes.length}</span>
+          {initialTweetAuthor && (
+            <div className="tweet-info__reply">
+              <Link to={`/tweet/${tweet.replyingTo}`}>
+                Replying to @{initialTweetAuthor}
+              </Link>
+            </div>
+          )}
+          <p>{tweet.text}</p>
+          <div className="tweet-icons">
+            {replyIcon}
+            <span>{tweet.replies.length}</span>
+            <button className="heart-button" onClick={toggleLike}>
+              {liked ? heartIconFilled : heartIcon}
+            </button>
+            <span>{tweet.likes.length}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
